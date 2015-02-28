5ea7fed246149b85c92127cb66ba02c54083bacb
szrapnel <szrapnel@poczta.onet.pl>
2015-02-28 19:30:59 +0000

    Task 1 (add age to user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates :age, :numericality => { :greater_than_or_equal_to => 0 }
 end
